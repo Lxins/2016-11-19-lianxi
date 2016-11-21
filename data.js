f5a9d3ec82b8ee74110cc02f04e2c1689d752df7
Lxins <www.1048185266.com@qq.com>
2016-11-21 19:10:21 +0800

    1

---
+++ data.js
@@ -0,0 +1,46 @@
+data = [
+	{
+		name: "海贼王",
+		obj: {
+			name: "热血",
+			child: [
+				{name: "路飞"},
+				{name: "索隆"},
+				{name: "香吉"}
+			]
+		}
+	},
+	{
+		name: "海贼王",
+		obj: {
+			name: "热血",
+			child: [
+				{name: "路飞"},
+				{name: "索隆"},
+				{name: "香吉"}
+			]
+		}
+	},
+	{
+		name: "海贼王",
+		obj: {
+			name: "热血",
+			child: [
+				{name: "路飞"},
+				{name: "索隆"},
+				{name: "香吉"}
+			]
+		}
+	},
+	{
+		name: "海贼王",
+		obj: {
+			name: "热血",
+			child: [
+				{name: "路飞"},
+				{name: "索隆"},
+				{name: "香吉"}
+			]
+		}
+	},
+];
